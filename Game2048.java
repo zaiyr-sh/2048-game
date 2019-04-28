059e2e34598b0b4df94d6aafb1f6c5d975dca2a2
Zaiyr <zaiyr.00@gmail.com>
2019-04-28 13:16:48 +0600

    All imports from Java

--- Game2048.java
+++ Game2048.java
@@ -15,7 +15,6 @@ public class Game2048 extends JPanel {
         game.setSize(340, 400);
         game.setResizable(false);
 
-
         game.add(new Game2048());
 
         game.setLocationRelativeTo(null);
