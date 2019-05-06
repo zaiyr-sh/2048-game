d1ff6861175ebe29d977eb91625d25d14c145b1b
Zaiyr <zaiyr.00@gmail.com>
2019-05-07 00:47:58 +0600

    All imports from Java

--- src/MainPage.java
+++ src/MainPage.java
@@ -1,10 +1,12 @@
 package src;
-import src.PlayMusic;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainPage extends JFrame {
+    private static final Color bgColor = new Color(155, 62, 151);
     public MainPage(){
         setLocationRelativeTo(null);
         setVisible(true);
@@ -12,18 +14,40 @@ public class MainPage extends JFrame {
 
     public static void main(String[] args) {
         JFrame mainPage = new MainPage();
-        JButton button = new JButton(new ImageIcon("E:\\IAAU\\2048-game\\src\\Button image.jpg"));
-        button.setBounds(150,400,105, 40);//125 300 100 40
         mainPage.setTitle("2048");
         mainPage.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        mainPage.setSize(440, 500);
         mainPage.setResizable(false);
-        mainPage.add(button);
         mainPage.setLayout(null);
+        mainPage.setSize(440, 500);
+
+        JButton button = new JButton(new ImageIcon("E:\\IAAU\\2048-game\\src\\Button image.jpg"));
+        button.setBounds(150,400,105, 40);//125 300 100 40
+        mainPage.add(button);
+
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Game2048 game2048 = new Game2048();
+                game2048.setVisible(true);
+            }
+        });
         mainPage.setVisible(true);
+
+
+        JButton quit = new JButton("Quit Game");
+        quit.setBounds(300,100,105, 40);//125 300 100 40
+        mainPage.add(quit);
+        quit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Quit the program
+                System.exit(0);
+            }
+        });
+        quit.setBackground(Color.LIGHT_GRAY);
         //mainPage.add(new MainPage());
 
-
+        mainPage.setLocationRelativeTo(null); // the same(null) position of the game
         PlayMusic.playMusic("src\\background music.wav");
     }
 }
