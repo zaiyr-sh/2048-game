42e86266d071ef333a14335db8da11cad8c68285
Zaiyr <zaiyr.00@gmail.com>
2019-05-18 22:40:18 +0600

    All imports from Java

--- src/Rulles.java
+++ src/Rulles.java
@@ -8,8 +8,18 @@ import java.awt.event.ActionListener;
 public class Rulles extends JFrame {
     public Rulles() {
         setSize(new Dimension(340, 400));
-        setResizable(false);
+        rulle();
+    }
 
+    public void rulle() {
+        setSize(new Dimension(340, 400));
+        setFocusable(true); // move of figures
+        setResizable(false);
+        add(new JLabel());
+    }
+
+    public void paint(Graphics g){
+        g.drawString("Hello to JavaTutorial.net", 10, 10);
     }
 
     public static void main(String[] args) {
@@ -19,21 +29,38 @@ public class Rulles extends JFrame {
         rullesGame.setSize(340, 400);
         rullesGame.setResizable(false);
 
-        rullesGame.add(new Rulles());
-        rullesGame.setLocationRelativeTo(null);
-        rullesGame.setVisible(true);
+        rullesGame.getContentPane().setBackground(Color.cyan);
+
 
         JButton back = new JButton("Back");
-        rullesGame.getContentPane().add(back);
-        back.setBounds(125,200,100, 40);
+        back.setLayout(null);
+        back.setBounds(125,300,100, 40);//125 300 100 40
         back.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                MainPage mainPage = new MainPage();
-                mainPage.setLocationRelativeTo(null);
-                mainPage.setVisible(true);
+                // Quit the program
+                System.exit(0);
             }
         });
         back.setBackground(Color.LIGHT_GRAY);
+        rullesGame.add(back);
+
+        String rullesOfGame = "<html><body width='%1s'><h1 align=\"center\">How to play?</h1>"
+                + "2048 is an addictive puzzle game based on simple addition. The objective of the game is to reach 2048 by merging adjacent similar number tiles on a 4x4 board. The game starts with two tiles of 2 random positions on the board, and continues to add new tiles of 2 or 4 on random tiles after every move. The game ends when your board is completely filled with numbers and you don’t have any move left.";
+
+        JLabel rulLabel = new JLabel(rullesOfGame,JLabel.CENTER);
+        rulLabel.setFont((new Font("Times New Roman", Font.CENTER_BASELINE, 15)));
+        rulLabel.setAlignmentX(0);
+        rulLabel.setAlignmentY(0);
+        rullesGame.add(rulLabel);
+
+
+
+//        rullesGame.add(new Rulles());
+        rullesGame.setLocationRelativeTo(null);
+        rullesGame.setVisible(true);
+
+
     }
 }
+
